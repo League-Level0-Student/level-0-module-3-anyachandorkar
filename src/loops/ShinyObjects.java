8eec6d62849858c71933a0dc70ca7f933c1181de
Anya <Anya@AnyaCyber>
2018-01-14 21:41:47 -0800

    BananaQuiz, ShinyObjects, WalkOfFame

--- src/loops/ShinyObjects.java
+++ src/loops/ShinyObjects.java
@@ -5,18 +5,21 @@ package loops;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 
 public class ShinyObjects {
 	public static void main(String[] args) {
 
 		// 2. Ask the user how many shiny objects they want
-
+String userobj = JOptionPane.showInputDialog("How many shiny objects do you want?");
 		// 3. Play the sound that many times
-
+int numbobj = Integer.parseInt(userobj);
 		// 1. Call the method below
-
-	}
+for (int i = 0; i < numbobj; i++) {
+	
+playMisterZee();
+	}}
 
 	public static void playMisterZee() {
     	try {
--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -4,6 +4,8 @@
 
 package loops;
 
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
@@ -11,16 +13,28 @@ public class WalkOfFame {
 		Robot rob = new Robot();
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
+rob.setX(50);
+rob.setSpeed(10);
+rob.penDown();
+rob.setPenColor(Color.BLUE);
 		// 2. Make the robot draw a star shape. Hint: 144.
-
+rob.turn(45);
+rob.move(150);
+rob.turn(100);
+rob.move(150);
+rob.turn(155);
+rob.move(190);
+rob.turn(155);
+rob.move(160);
+rob.turn(165);
+rob.move(190);
 		// 3. Set the size of the star to 30.
 
 		/** THE CHALLENGE: **/
 		/*
 		 * Make the robot draw a line of stars like this:
 		 * http://bit.ly/walk-of-fame
-		 * 
+		 
 		 * Hint: The distance between stars is 50.
 		 */
 
